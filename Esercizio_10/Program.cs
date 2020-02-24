fc60148c3f7f2d6437ac2b1594b7cd4f005d3229
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2020-02-24 09:52:42 +0100

    Add project files.

---
+++ Esercizio_10/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using Metodo;
+
+namespace Esercizio_10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random r = new Random();
+            Console.WriteLine("Inserisci la lunghezza dell'array: ");
+            int n = int.Parse(Console.ReadLine());
+            int[] a = new int[n];
+            int numero;
+            List<int> lista = new List<int>();
+            for (int i = 0; i < a.Length; i++)
+            {
+                do
+                {
+                    numero = r.Next();
+                } while (lista.Contains(numero));
+                lista.Add(numero);
+                a[i] = numero;
+            }
+            a = CountingSort.Sort(a);
+            Console.WriteLine("Array ordinato:");
+            for ( int i = 0; i < a.Length; i++)
+            {
+                Console.WriteLine(a[i]);
+            }
+            Console.ReadLine();
+        }
+    }
+}
